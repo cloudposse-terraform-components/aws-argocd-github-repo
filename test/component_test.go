6f129406d72c555025b18919b2dc957c983059da
Igor Rodionov <goruha@gmail.com>
2025-04-02 11:03:09 +0200

    Added asserts

--- test/component_test.go
+++ test/component_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"context"
 	"fmt"
 	"os"
 	// "strconv"
@@ -10,7 +11,7 @@ import (
 	helper "github.com/cloudposse/test-helpers/pkg/atmos/component-helper"
 	awsTerratest "github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/random"
-	// "github.com/jferrl/go-githubauth"
+	"github.com/google/go-github/v70/github"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -22,12 +23,14 @@ func (s *ComponentSuite) TestBasic() {
 	const component = "argocd-github-repo/basic"
 	const stack = "default-test"
 	const awsRegion = "us-east-2"
+	const githubOrg = "cloudposse-tests"
 
 	token := os.Getenv("GITHUB_TOKEN")
 
 	randomID := strings.ToLower(random.UniqueId())
 
 	secretPath := fmt.Sprintf("/argocd/%s/github/api_key", randomID)
+	deployKeyPath := fmt.Sprintf("/argocd/deploy_keys/%s/%s", randomID, "%s")
 	repoName := fmt.Sprintf("argocd-github-repo-%s", randomID)
 
 	defer func() {
@@ -36,14 +39,27 @@ func (s *ComponentSuite) TestBasic() {
 	awsTerratest.PutParameter(s.T(), awsRegion, secretPath, "Github API Key", token)
 
 	inputs := map[string]interface{}{
+		"ssm_github_deploy_key_format": deployKeyPath,
 		"ssm_github_api_key": secretPath,
-		"name":                repoName,
+		"name": repoName,
+		"github_organization": githubOrg,
 	}
 
 	defer s.DestroyAtmosComponent(s.T(), component, stack, &inputs)
 	options, _ := s.DeployAtmosComponent(s.T(), component, stack, &inputs)
 	assert.NotNil(s.T(), options)
 
+	client := github.NewClient(nil).WithAuthToken(token)
+
+	// Check if the repository exists
+	repo, _, err := client.Repositories.Get(context.Background(), githubOrg, repoName)
+	assert.Nil(s.T(), err)
+	assert.NotNil(s.T(), repo)
+
+	filePath := "mgmt/uw2-sandbox/argocd/applicationset.yaml"
+	// Use the GitHub API to check if the file exists in the repository
+	_, _, _, err = client.Repositories.GetContents(context.Background(), githubOrg, repoName, filePath, nil)
+	assert.Nil(s.T(), err)
 	s.DriftTest(component, stack, &inputs)
 }
 
--- test/go.mod
+++ test/go.mod
@@ -93,8 +93,9 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/gnostic-models v0.6.9 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/go-github/v69 v69.2.0 // indirect
+	github.com/google/go-github/v70 v70.0.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
